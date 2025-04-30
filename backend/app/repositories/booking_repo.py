86a92a67b673b8393018a496b5fe214045662836
Egor <fisher12235@mail.ru>
2025-04-30 11:43:48 +0700

    booking repo

---
+++ backend/app/repositories/booking_repo.py
@@ -0,0 +1,47 @@
+from sqlalchemy import select,insert,update,delete
+from app.schemas.booking_schema import BookingSchema
+from app.models.temp_models import BookingModel
+from app.config.session import Session
+from datetime import datetime
+
+
+class BookingRepository:
+
+    @staticmethod
+    def get_booking_by_userid(user_id: int,):
+        with Session() as session:
+            stmt = select(BookingModel).where(BookingModel.user_id == user_id)
+            result = session.execute(stmt).scalar_one_or_none()
+            return result
+        
+    @staticmethod
+    def get_all_booking_by_userid(user_id: int):
+        with Session() as session:
+            stmt = select(BookingModel).where(BookingModel.user_id == user_id)
+            result = session.execute(stmt).fetchall()
+            return result
+        
+    
+    @staticmethod
+    def create_booking(start_date: datetime,end_date: datetime, hotel_id: int, room_id: int,user_id):
+        with Session() as session:
+            new_booking = BookingModel(
+                start_date=start_date,
+                end_date=end_date,
+                hotel_id=hotel_id,
+                room_id=room_id,
+                user_id=user_id
+            )
+
+            session.add(new_booking)
+            session.commit()
+
+            return new_booking
+        
+    
+    @staticmethod
+    def delete_booking(user_id: int):
+        with Session() as session:
+            stmt = delete(BookingModel).where(BookingModel.user_id == user_id)
+            result = session.execute(stmt)
+            return result
