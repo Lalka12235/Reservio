0197a7060e9e93c7576566e0048d820cac19b0ec
Egor <fisher12235@mail.ru>
2025-07-16 12:17:48 +0700

    new feature in hotel_repo

--- backend/app/repositories/hotel_repo.py
+++ backend/app/repositories/hotel_repo.py
@@ -1,6 +1,6 @@
 import logging
 from sqlalchemy import select,delete, update
-from app.config.session import Session
+from sqlalchemy.orm import Session
 from app.models.hotel_model import HotelModel
 from app.schemas.hotel_schema import HotelSchema
 
@@ -9,43 +9,39 @@ logger = logging.getLogger(__name__)
 class HotelRepository:
 
     @staticmethod
-    def get_hotel_by_title(title: str):
-        with Session() as session:
-            stmt = select(HotelModel).where(HotelModel.title == title)
-            result = session.execute(stmt).scalar_one_or_none()
-            logger.info(f"Получен отель с названием: {title}")
-            return result
+    def get_hotel_by_title(db: Session,title: str):
+        stmt = select(HotelModel).where(HotelModel.title == title)
+        result = db.execute(stmt).scalar_one_or_none()
+        logger.info(f"Получен отель с названием: {title}")
+        return result
 
     @staticmethod
-    def create_info_about_hotel(hotel: HotelSchema):
-        with Session() as session:
-            new_hotel = HotelModel(
-                title=hotel.title,
-                description=hotel.description,
-            )
-            session.add(new_hotel)
-            session.commit()
-            logger.info(f"Создан отель: {hotel.title}")
-            return new_hotel.id
+    def create_info_about_hotel(db: Session,hotel: HotelSchema):
+        new_hotel = HotelModel(
+            title=hotel.title,
+            description=hotel.description,
+        )
+        db.add(new_hotel)
+        db.commit()
+        logger.info(f"Создан отель: {hotel.title}")
+        return new_hotel.id
 
     @staticmethod
-    def update_info_about_hotel(old_title: str, hotel: HotelSchema):
-        with Session() as session:
-            stmt = (
-                update(HotelModel)
-                .where(HotelModel.title == old_title)
-                .values(title=hotel.title, description=hotel.description)
-            )
-            result = session.execute(stmt)
-            session.commit()
-            logger.info(f"Обновлён отель: {old_title} -> {hotel.title}")
-            return result
+    def update_info_about_hotel(db: Session,old_title: str, hotel: HotelSchema):
+        stmt = (
+            update(HotelModel)
+            .where(HotelModel.title == old_title)
+            .values(title=hotel.title, description=hotel.description)
+        )
+        result = db.execute(stmt)
+        db.commit()
+        logger.info(f"Обновлён отель: {old_title} -> {hotel.title}")
+        return result
 
     @staticmethod
-    def delete_hotel(title: str):
-        with Session() as session:
-            stmt = delete(HotelModel).where(HotelModel.title == title)
-            result = session.execute(stmt)
-            session.commit()
-            logger.info(f"Удалён отель с названием: {title}")
-            return result
+    def delete_hotel(db: Session,title: str):
+        stmt = delete(HotelModel).where(HotelModel.title == title)
+        result = db.execute(stmt)
+        db.commit()
+        logger.info(f"Удалён отель с названием: {title}")
+        return result
