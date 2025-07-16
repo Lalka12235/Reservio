70a4df9f64ee0913a679a52bc406ac0dd9d1ea48
Egor <fisher12235@mail.ru>
2025-07-16 12:17:32 +0700

    new feature in room_category_repo

--- backend/app/repositories/room_category_repo.py
+++ backend/app/repositories/room_category_repo.py
@@ -1,70 +1,62 @@
-from sqlalchemy import select,insert,update,delete
+from sqlalchemy import select,delete
 
-from app.config.session import Session
+from sqlalchemy.orm import Session
 from app.schemas.room_category_schema import RoomCategorySchema
 
 from app.models.room_category_model import RoomCategoryModel
 
-from app.services.hotel_service import HotelService
 
 
 class RoomCategoryRepository:
 
     @staticmethod
-    def get_all_room_category_by_hotel(hotel_id: int):
-        with Session() as session:
-
-            stmt = select(RoomCategoryModel).where(RoomCategoryModel.hotel_id == hotel_id)
-            result = session.execute(stmt).fetchall()
-            return result
+    def get_all_room_category_by_hotel(db: Session,hotel_id: int):
+        stmt = select(RoomCategoryModel).where(RoomCategoryModel.hotel_id == hotel_id)
+        result = db.execute(stmt).fetchall()
+        return result
         
     
     @staticmethod
-    def get_room_category_by_title(hotel_id,title_category):
-        with Session() as session:
-            stmt = select(RoomCategoryModel).where(
-                RoomCategoryModel.hotel_id == hotel_id, 
-                RoomCategoryModel.title == title_category,
-                )
-            
-            result = session.execute(stmt)
-            return result
-        
-    
-    @staticmethod
-    def create_room_category(hotel_id: int,category: RoomCategorySchema):
-        with Session() as session:
-            new_room_category = RoomCategoryModel(
-                title_category=category.title,
-                description=category.description,
-                hotel_id=hotel_id,
+    def get_room_category_by_title(db: Session,hotel_id,title_category):
+        stmt = select(RoomCategoryModel).where(
+            RoomCategoryModel.hotel_id == hotel_id, 
+            RoomCategoryModel.title == title_category,
             )
-
-            session.add(new_room_category)
-            session.commit()
-            
-            return new_room_category
         
-    @staticmethod
-    def update_room_category(category: RoomCategorySchema):
-        with Session() as session:
-            upd_room_category = RoomCategoryModel(
-                title_category=category.title,
-                description=category.description,
-            )
-
-            session.add(upd_room_category)
-            session.commit()
-
-            return upd_room_category
+        result = db.execute(stmt)
+        return result
         
     
     @staticmethod
-    def delete_room_category(hotel_id: int,title_category):
-        with Session() as session:
+    def create_room_category(db: Session,hotel_id: int,category: RoomCategorySchema):
+        new_room_category = RoomCategoryModel(
+            title_category=category.title,
+            description=category.description,
+            hotel_id=hotel_id,
+        )
 
-            stmt = delete(RoomCategoryModel).where(RoomCategoryModel.hotel_id == hotel_id, RoomCategoryModel.title == title_category)
-            session.execute(stmt)
-            return hotel_id,title_category
+        db.add(new_room_category)
+        db.commit()
+        
+        return new_room_category
+        
+    @staticmethod
+    def update_room_category(db: Session,category: RoomCategorySchema):
+        upd_room_category = RoomCategoryModel(
+            title_category=category.title,
+            description=category.description,
+        )
+
+        db.add(upd_room_category)
+        db.commit()
+
+        return upd_room_category
+        
+    
+    @staticmethod
+    def delete_room_category(db: Session,hotel_id: int,title_category):
+        stmt = delete(RoomCategoryModel).where(RoomCategoryModel.hotel_id == hotel_id, RoomCategoryModel.title == title_category)
+        db.execute(stmt)
+        return hotel_id,title_category
         
     
