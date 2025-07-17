7ba0196b34f8d40e0fce9af2824ed75f9af4fa70
Egor <fisher12235@mail.ru>
2025-07-17 15:34:20 +0700

    new feature in room_service

--- backend/app/services/room_service.py
+++ backend/app/services/room_service.py
@@ -4,14 +4,15 @@ from app.schemas.room_schema import RoomSchema
 
 from app.services.hotel_service import HotelService
 from app.services.room_category_service import RoomCategoryService
+from sqlalchemy.orm import Session
 
 class RoomService:
 
     @staticmethod
-    def get_room_by_title(title_hotel: str,title_room: str):
-        hotel = HotelService.get_hotel_by_title(title_hotel)
+    def get_room_by_title(db: Session,title_hotel: str,title_room: str):
+        hotel = HotelService.get_hotel_by_title(db,title_hotel)
 
-        room = RoomRepository.get_room_by_title(hotel,title_room)
+        room = RoomRepository.get_room_by_title(db,hotel,title_room)
 
         if not room:
             raise HTTPException(
@@ -22,10 +23,10 @@ class RoomService:
         return {'message': 'success','detail': room}
     
     @staticmethod
-    def get_all_room_by_hotel(title_hotel: str):
-        hotel = HotelService.get_hotel_by_title(title_hotel)
+    def get_all_room_by_hotel(db: Session,title_hotel: str):
+        hotel = HotelService.get_hotel_by_title(db,title_hotel)
 
-        rooms = RoomRepository.get_all_room_by_hotel(hotel)
+        rooms = RoomRepository.get_all_room_by_hotel(db,hotel)
 
         if not rooms:
             raise HTTPException(
@@ -36,12 +37,12 @@ class RoomService:
         return {'message': 'success','rooms':{[room for room in rooms]}}
     
     @staticmethod
-    def create_room(room: RoomSchema,title_hotel: str, title_category: str):
-        hotel = HotelService.get_hotel_by_title(title_hotel)
+    def create_room(db: Session,room: RoomSchema,title_hotel: str, title_category: str):
+        hotel = HotelService.get_hotel_by_title(db,title_hotel)
 
-        category = RoomCategoryService.get_room_category_by_title(hotel,title_category)
+        category = RoomCategoryService.get_room_category_by_title(db,hotel,title_category)
 
-        result = RoomRepository.create_room(room,hotel,category.id)
+        result = RoomRepository.create_room(db,room,hotel,category.id)
 
         if not result:
             raise HTTPException(
@@ -52,12 +53,12 @@ class RoomService:
         return {'message': 'success','create': result}
     
     @staticmethod
-    def update_room(old_title: str, room: RoomSchema, title_category: str):
-        hotel = HotelService.get_hotel_by_title(old_title)
+    def update_room(db: Session,old_title: str, room: RoomSchema, title_category: str):
+        hotel = HotelService.get_hotel_by_title(db,old_title)
 
-        category = RoomCategoryService.get_room_category_by_title(hotel,title_category)
+        category = RoomCategoryService.get_room_category_by_title(db,hotel,title_category)
 
-        result = RoomRepository.update_room(old_title,room,category.id,hotel)
+        result = RoomRepository.update_room(db,old_title,room,category.id,hotel)
 
         if not result:
             raise HTTPException(
@@ -69,10 +70,10 @@ class RoomService:
     
 
     @staticmethod
-    def delete_room(title_room: str, title_hotel: str):
-        hotel = HotelService.get_hotel_by_title(title_hotel)
+    def delete_room(db: Session,title_room: str, title_hotel: str):
+        hotel = HotelService.get_hotel_by_title(db,title_hotel)
 
-        result = RoomRepository.delete_room(title_room,hotel)
+        result = RoomRepository.delete_room(db,title_room,hotel)
 
         if not result:
             raise HTTPException(
