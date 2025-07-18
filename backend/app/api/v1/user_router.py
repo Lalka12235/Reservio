fa739a62d8547ae98be7be1eed81e1555744295e
Egor <fisher12235@mail.ru>
2025-07-18 12:11:24 +0700

    new feature in user_api

--- backend/app/api/v1/user_router.py
+++ backend/app/api/v1/user_router.py
@@ -1,4 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter,Depends
+from app.config.session import get_db
+from typing import Annotated
+from sqlalchemy.orm import Session
 
 
 from app.services.user_service import UserServices
@@ -9,18 +12,18 @@ user = APIRouter(
     tags=['User']
 )
 
-
+db_dependency = Annotated[Session,Depends(get_db)]
 
 @user.get('/{username}')
-async def get_user(username: str):
-    return UserServices.get_user(username)
+async def get_user(db:db_dependency,username: str):
+    return UserServices.get_user(db,username)
 
 
 @user.post('/register')
-async def register_user(user: UserRegisterSchema):
-    return UserServices.register_user(user)
+async def register_user(db:db_dependency,user: UserRegisterSchema):
+    return UserServices.register_user(db,user)
 
 
 @user.delete('/{username}')
-async def delete_user(user: UserLoginSchema, username: str):
-    return UserServices.delete_user(user)
+async def delete_user(db:db_dependency,user: UserLoginSchema, username: str):
+    return UserServices.delete_user(db,user)
