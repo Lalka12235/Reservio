7d4467bb002bcedd15839d6475e3945300d34a32
Egor <fisher12235@mail.ru>
2025-07-18 12:12:26 +0700

    new feature in hotel_api

--- backend/app/api/v1/hotel_router.py
+++ backend/app/api/v1/hotel_router.py
@@ -1,4 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter,Depends
+from app.config.session import get_db
+from typing import Annotated
+from sqlalchemy.orm import Session
 
 from app.services.hotel_service import HotelService
 from app.schemas.hotel_schema import HotelSchema
@@ -9,23 +12,23 @@ hotel = APIRouter(
     tags=['Hotel']
 )
 
-
+db_dependency = Annotated[Session,Depends(get_db)]
 
 @hotel.get('/{title}')
-async def get_hotel(title: str):
-    return HotelService.get_hotel_by_title(title)
+async def get_hotel(db:db_dependency,title: str):
+    return HotelService.get_hotel_by_title(db,title)
 
 
 @hotel.post('/')
-async def create_info_about_hotel(hotel_data: HotelSchema):
-    return HotelService.create_info_about_hotel(hotel_data)
+async def create_info_about_hotel(db:db_dependency,hotel_data: HotelSchema):
+    return HotelService.create_info_about_hotel(db,hotel_data)
 
 
 @hotel.put('/{old_title}')
-async def update_info_about_hotel(old_title: str, hotel_data: HotelSchema):
-    return HotelService.update_info_about_hotel(old_title,hotel_data)
+async def update_info_about_hotel(db:db_dependency,old_title: str, hotel_data: HotelSchema):
+    return HotelService.update_info_about_hotel(db,old_title,hotel_data)
 
 
 @hotel.delete('/{title}')
-async def delete_hotel(title: str):
-    return HotelService.delete_hotel(title)
+async def delete_hotel(db:db_dependency,title: str):
+    return HotelService.delete_hotel(db,title)
